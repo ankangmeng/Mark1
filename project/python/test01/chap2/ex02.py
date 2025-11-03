8dfd4267eeb55e332764ed410fa5ba58cc52bcd9
ankangmeng <142792260+ankangmeng@users.noreply.github.com>
2025-11-03 12:28:57 +0800

    Add files via upload

---
+++ project/python/test01/chap2/ex02.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+
+age=input("input your age:")
+saying=input("input your saying:")
+print("-------自我介绍-------")
+print("姓名："+name)
+print("年龄:"+age)
+print("座右铭:"+saying)
